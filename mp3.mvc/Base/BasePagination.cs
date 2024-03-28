355fe4bf5e2ed4de5a5e2aa9771e6447bcf6d472
VTI\anh.canviet <anh.canviet@vti.com.vn>
2024-03-28 18:15:56 +0700

    add collection page

---
+++ mp3.mvc/Base/BasePagination.cs
@@ -0,0 +1,20 @@
+﻿namespace mp3.mvc.Base
+{
+    public class BasePagination<T>
+    {
+        public BasePagination(long totalPage, long totalItems, long page, long pageSize, IEnumerable<T> items)
+        {
+            TotalPage = totalPage;
+            TotalItems = totalItems;
+            Page = page;
+            PageSize = pageSize;
+            Items = items;
+        }
+
+        public long TotalPage { get; set; }
+        public long TotalItems { get; set; }
+        public long Page { get; set; }
+        public long PageSize { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
